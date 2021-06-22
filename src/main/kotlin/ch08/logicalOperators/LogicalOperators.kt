9a004386af9af0ac81ddea8151828dd16e407840
choy-ys <project.log.062@gmail.com>
2021-06-22 14:59:07 +0900

    [2021-06-22]용석:Kotlin 논리 연산자 예제(and, or, not)

---
+++ src/main/kotlin/ch08/logicalOperators/LogicalOperators.kt
@@ -0,0 +1,16 @@
+package ch08.logicalOperators
+
+/**
+ * @author : choi-ys
+ * @date : 2021/06/14 1:53 오후
+ * @apiNote : 논리 연산자 = 논리 값을 이용하여 새로운 논리 값을 도출
+ *  - && : and
+ *  - || : or
+ *  - ! : not
+ */
+fun main(){
+    println("true && false : " + (true && false))
+    println("true || false : " + (true || false))
+    println("!true : " + !true)
+    println("!false : " + !false)
+}
