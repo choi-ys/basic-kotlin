b17d571c9a89d6b8e7c6139efc587ddd353669ba
choy-ys <project.log.062@gmail.com>
2021-06-23 15:38:03 +0900

    [2021-06-23]용석:Kotlin 변수를 다루는 방법 #3 (lazy delegate properties)

--- src/main/kotlin/ch28/variableHandling/lateinit/Lateinit.kt
+++ src/main/kotlin/ch28/variableHandling/lateinit/Lateinit.kt
@@ -3,7 +3,7 @@ package ch28.variableHandling.lateinit
 /**
  * @author : choi-ys
  * @date : 2021/06/23 3:04 오후
- * @apiNote : Kotlin 늦은 초기화  : lateinit
+ * @apiNote : Kotlin 늦은 초기화 : lateinit
  */
 fun main() {
     var lateInitExample: LateInitExample = LateInitExample()
