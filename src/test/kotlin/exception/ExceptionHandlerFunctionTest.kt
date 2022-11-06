7dd2faa6cde3f96000e1745779c051ff3a465b91
choi-ys <project.log.062@gmail.com>
2022-11-06 23:21:15 +0900

    test: Kotlin의 예외 처리 함수 require(), requireNotNull(), check(), checkNotNull()에 대한 학습 테스트 작성

--- src/test/kotlin/exception/ExceptionHandlerFunctionTest.kt
+++ src/test/kotlin/exception/ExceptionHandlerFunctionTest.kt
@@ -1,9 +1,23 @@
 package exception
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 
-class RequireTest : StringSpec({
-    "require" {
-        
+internal class ExceptionHandlerFunctionTest : StringSpec({
+    "require()는 매개변수 값이 거짓인 경우, IllegalArgumentException을 발생시킨다." {
+        val given = "".isNotEmpty()
+        shouldThrow<IllegalArgumentException> { require(given) { "Required function is false" } }
+    }
+
+    "requireNotNull()는 매개변수 값이 null인 경우, IllegalArgumentException을 발생시킨다." {
+        shouldThrow<IllegalArgumentException> { requireNotNull(null) { "Required value is null" } }
+    }
+
+    "check()는 매개변수 값이 거짓인 경우, IllegalStateException을 발생시킨다." {
+        shouldThrow<IllegalStateException> { check(false) { "Required value is false" } }
+    }
+
+    "checkNotNull()는 매개변수 값이 null인 경우, IllegalStateException을 발생시킨다." {
+        shouldThrow<IllegalStateException> { checkNotNull(null) { "Required value is false" } }
     }
 })
